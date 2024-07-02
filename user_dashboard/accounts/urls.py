d7e78939a93627da5181f2653213ca1232c32ac8
Bhavsar Aesha <101286259+BhavsarAesha@users.noreply.github.com>
2024-07-02 18:39:53 +0530

    Add files via upload

---
+++ user_dashboard/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import SignUpView, login_view, dashboard_view, HomeView
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', login_view, name='login'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('', HomeView.as_view(), name='home'),   
+    # path('home/', HomeView.as_view(), name='home'),      
+]
---
+++ user_dashboard/accounts/views.py
@@ -0,0 +1,53 @@
+
+# Create your views here.
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm, UserLoginForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
+from .models import CustomUser
+
+
+
+class SignUpView(CreateView):
+    model = CustomUser
+    form_class = UserRegisterForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('dashboard')
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard')
+            else:
+                messages.error(request, 'Invalid username or password')
+    else:
+        form = UserLoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+class HomeView(TemplateView):
+    template_name = 'accounts/home.html'
+
+@login_required
+def dashboard_view(request):
+    if request.user.user_type == 'patient':
+        return render(request, 'accounts/dashboard_patient.html', {'user': request.user})
+    else:
+        return render(request, 'accounts/dashboard_doctor.html', {'user': request.user})
+
